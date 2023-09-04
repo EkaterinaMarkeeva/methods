ab28aef275530b78d64d1340355ae303eb951882
Ekaterina Markeeva <lesekaterina25@gmail.com>
2023-09-04 11:19:59 +0400

    optimization of the constructor Character

--- src/character.js
+++ src/character.js
@@ -1,14 +1,19 @@
 export default class Character {
   constructor(name, type) {
+    const types = ['Bowman', 'Daemon', 'Magician', 'Swordsman', 'Undead', 'Zombie'];
+    
     if (typeof name !== 'string' || name.length < 2 || name.length > 10) {
       throw new Error ("Имя должно быть не менее 2 и не более 10 символов");
-    }
-    if (type !== 'Bowman' && type !== 'Daemon' && type !== 'Magician' && type !== 'Swordsman' && type !== 'Undead'&& type !== 'Zombie') {
-      throw new Error ("Тип не найден");
+    } else {
+      this.name = name;
     }
 
-    this.name = name;
-    this.type = type;
+    if (!types.includes(type)) {
+      throw new Error ("Тип не найден");
+    } else {
+      this.type = type;
+    }
+
     this.health = 100;
     this.level = 1;
     this.attack = undefined;
